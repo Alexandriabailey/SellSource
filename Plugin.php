f93bcba331f4ac3b944cb59fa1b9aec54ab8fea2
Alexandria Bailey <Alexandriadesigns@execs.com>
2023-07-01 13:59:37 -0500

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,17 +1,23 @@
 <?php
 
-namespace YourPluginNamespace;
+namespace SellSource;
 
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use onebone\economyapi\EconomyAPI;
+use pocketmine\item\Item;
+use pocketmine\Server;
 
 class SellPlugin extends PluginBase {
 
     public function onEnable() {
-        // You can add any other code you need to run when the plugin is enabled
+        $economyPlugin = $this->getEconomyAPIPlugin();
+
+        if ($economyPlugin === null) {
+            $this->getLogger()->error("This plugin requires EconomyAPI-PM5_dev-1.");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
@@ -25,20 +31,51 @@ class SellPlugin extends PluginBase {
             return true;
         }
         
-        $amount = (float) $args[0];
-        $itemName = "your_item_name"; // Replace with the actual name of the item you want to sell
-        $sellPrice = 10; // Replace with the sell price for the item
+        $economy = $this->getEconomyAPIPlugin();
+
+        if ($economy === null) {
+            $sender->sendMessage("This plugin requires EconomyAPI-PM5_dev-1.");
+            return true;
+        }
         
-        $economy = EconomyAPI::getInstance();
+        $amount = (int) $args[0];
+        $item = $sender->getInventory()->getItemInHand();
+        $itemName = $item->getName();
+        $sellPrice = $item->getSellPrice(); // Assuming you have implemented this method to retrieve the item's sell price
         
-        if ($economy->reduceMoney($sender, $amount * $sellPrice)) {
+        if ($sellPrice === 0) {
+            $sender->sendMessage("This item cannot be sold.");
+            return true;
+        }
+        
+        if ($amount > $item->getCount()) {
+            $amount = $item->getCount();
+        }
+        
+        $totalSellPrice = $amount * $sellPrice;
+        
+        if ($economy->reduceMoney($sender, $totalSellPrice)) {
             // Sell successful
-            $sender->sendMessage("Sold " . $amount . " " . $itemName . " for " . ($amount * $sellPrice) . " coins.");
+            $item->setCount($item->getCount() - $amount);
+            $sender->getInventory()->setItemInHand($item);
+            $economy->addMoney($sender, $totalSellPrice);
+            
+            $sender->sendMessage("Sold " . $amount . " " . $itemName . " for " . $totalSellPrice . " coins.");
         } else {
             // Insufficient funds
             $sender->sendMessage("Insufficient funds.");
         }
         
         return true;
+    }
+
+    private function getEconomyAPIPlugin(): ?PluginBase {
+        $economyPlugin = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+        
+        if ($economyPlugin instanceof PluginBase && $economyPlugin->getDescription()->getVersion() === "PM5_dev-1") {
+            return $economyPlugin;
+        }
+        
+        return null;
     }
 }
